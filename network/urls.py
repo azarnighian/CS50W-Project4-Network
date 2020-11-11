77dc7ab22e53b884b6f9626075c194b2371acc05
Aharon <aharonzarnighian@gmail.com>
2020-11-11 11:56:02 -0500

    Finished Following spec

--- network/urls.py
+++ network/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("profile/<str:username>", views.profile, name="profile"),
     path("follow/<str:this_username>", views.follow, name="follow"),
     path("unfollow/<str:this_username>", views.unfollow, name="unfollow"),
+    path("following", views.following, name="following"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
